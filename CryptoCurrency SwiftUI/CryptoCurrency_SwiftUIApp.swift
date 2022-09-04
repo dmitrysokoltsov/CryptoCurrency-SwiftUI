91e4f217ed11eee7a6a8316811fa3bc9628f3683
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2022-09-05 00:39:14 +0300

    Initial Commit

---
+++ CryptoCurrency SwiftUI/CryptoCurrency_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrency_SwiftUIApp.swift
+//  CryptoCurrency SwiftUI
+//
+//  Created by Dmitry Sokoltsov on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrency_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
