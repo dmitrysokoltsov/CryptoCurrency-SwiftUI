c60d2e80d55263bb389bad8b1b39ffd88fc35d67
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2022-09-23 16:23:46 +0300

    extortion MainView

--- CryptoCurrency SwiftUI/Core/Home/View/MainView.swift
+++ CryptoCurrency SwiftUI/Core/Home/View/MainView.swift
@@ -2,29 +2,44 @@ import SwiftUI
 
 struct MainView: View {
     
-    @State private var showButton: Bool = false
+    @State private var showButton: Bool = true
     
     var body: some View {
         ZStack {
             Color.theme.background
                 .ignoresSafeArea()
-            
             VStack {
-                HStack {
-                    TopButtonView(iconName: "info")
-                    Spacer()
-                    Text("Live Prices")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.theme.accent)
-                        
-                    Spacer()
-                    TopButtonView(iconName: "chevron.right")
-                }
-                .padding(.horizontal)
+                header
                 Spacer(minLength: 0)
             }
         }
+    }
+}
+
+extension MainView {
+    
+    private var header: some View {
+        HStack {
+            withAnimation(.none) {
+                TopButtonView(iconName: showButton ? "plus" : "info")
+                    .background(TopButtonAnimationView(animation: $showButton))
+            }
+            Spacer()
+            Text("Live Prices")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(Color.theme.accent)
+            
+            Spacer()
+            TopButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showButton ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showButton.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
     }
 }
 
