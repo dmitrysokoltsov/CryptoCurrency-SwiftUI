797cb4a3ccf8c5b71d9c355ae8c1a17699ebc65a
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2022-10-03 17:54:43 +0300

    Delete

---
+++ CryptoCurrency SwiftUI/Core/Home/View/PortfolioView.swift
@@ -0,0 +1,20 @@
+//
+//  PortfolioView.swift
+//  CryptoCurrency SwiftUI
+//
+//  Created by Dmitry Sokoltsov on 03.10.2022.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PortfolioView_Previews: PreviewProvider {
+    static var previews: some View {
+        PortfolioView()
+    }
+}
