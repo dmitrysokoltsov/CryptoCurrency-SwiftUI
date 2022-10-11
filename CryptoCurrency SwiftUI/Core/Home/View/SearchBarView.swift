86e62c750939b59e72efd8a9db8c1e43a9d9b482
DmitrySokoltsov <dmitrysokoltsov@gmail.com>
2022-10-11 23:36:19 +0300

    Search bar view

---
+++ CryptoCurrency SwiftUI/Core/Home/View/SearchBarView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBarView.swift
+//  CryptoCurrency SwiftUI
+//
+//  Created by Dmitry Sokoltsov on 11.10.2022.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView()
+    }
+}
